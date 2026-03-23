6f7d080161201b75fa739f02161c07b12c252c1a
rohitKumarSinha-dev <rohitgithubacc1@gmail.com>
2026-03-23 23:35:11 +0530

    learning Literals.

---
+++ basicPythonConcept/NotesConecptPractise.py
@@ -0,0 +1,26 @@
+
+z = 15 + 9.5j
+print (z)
+print(type(z))
+
+z1Value = complex(67.9, 122.82)
+print(z1Value)
+
+c = complex(10)
+print(c)
+
+
+# Now taking look into Literals , what are the type of literals, WHAT IS Literals
+# Integer , Float, Boolean, Complex, String
+#   ---> So the direct value that are used in the program are called as Literals
+#  --> LITERALS are also called as Constant
+#1 example
+a = 5
+b = 33
+
+# type of literals
+# -----> Integer Literals
+a = 201
+b = 232_103_197
+#  So if you want to write integer literals separated you can separate them with UNDERSCORE(IT is for our use , readability)
+
---
+++ basicPythonConcept/VariableConcept.py
@@ -0,0 +1,5 @@
+a = 7
+b = 5
+print(a,'and',b , "Is the Value of a and b")
+print(type(a))
+print(type(b))
