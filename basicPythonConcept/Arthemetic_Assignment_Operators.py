fa530f4550bd09382ef815952c2e6560791bd5c8
rohitKumarSinha-dev <rohitgithubacc1@gmail.com>
2026-03-26 20:43:45 +0530

    Topic 1- Arthemetic_Assignment Operators topic stydy

---
+++ basicPythonConcept/Arthemetic_Assignment_Operators.py
@@ -0,0 +1,66 @@
+print("What is Arithmetic Assignment Operator is:-"
+      " is a shorthand way to perform an arithmetic operation "
+      "and assign the result back to the same variable in one step")
+
+# Addition + Assignment +=
+print("<-----1(Addition + Assignment, +=)------>")
+b = 123
+x = 13
+a = 1
+c = 1
+c += a * (b * x)  #c = c + a *(b * x)
+print(c)
+
+print("<-----2(Substraction + Assignment,  -=)------>")
+# Substraction + Assignment -=
+b = 124
+x = 14
+a = 2
+c = 1
+c -= a * (b * x)  #c = c - a *(b * x)
+print(c)
+
+print("<-----3(Multiplication + Assignment,  *=)------>")
+# Multiplication + Assignment,  *=
+b = 125
+x = 15
+a = 3
+c = 6
+c *= a * (b * x)  #c = c * a *(b * x)
+print(c)
+
+print("<-----4(True division +  Assignment,  /=)------>")
+# True division +  Assignment,  /=
+b = 15
+x = 20
+a = 3.2
+c = 6
+c /= a * (b * x)  #c = c / a *(b * x)
+print(c)
+
+print("<-----5(Floor division + Assignment,  //=)------>")
+# Floor division + Assignment,  //=
+b = 1.5
+x = 2.5
+a = 3.2
+c = 6
+c //= a * (b * x)  #c = c // a *(b * x)
+print(c)
+
+print("<-----6(Modulo + Assignment,  %=)------>")
+# Modulo + Assignment,  %=
+b = 1.5
+x = 2.5
+a = 3.2
+c = 6
+c %= a * (b * x)  #c = c % a *(b * x)
+print(c)
+
+print("<-----7(Exponentiation + Assignment,  **=)------>")
+# Modulo + Assignment,  **=
+b = 1.235
+x = 2.52
+a = 3.292
+c = 6.222
+c **= a * (b * x)  #c = c **= a *(b * x)
+print(c)
